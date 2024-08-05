f7827c305b2103175ec0ab63ca1b03e7edf16cf8
Soumyajit Patra <soumyajitpatra72@gmail.com>
2024-08-05 20:07:35 +0530

    Add files via upload

---
+++ Week 2/Spring Core Maven/Exercise 1,2, 3/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public void performRepositoryAction() {
+        System.out.println("Repository action is being performed");
+    }
+}
---
+++ Week 2/Spring Core Maven/Exercise 1,2, 3/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,17 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void performService() {
+        System.out.println("Service is being performed");
+        bookRepository.performRepositoryAction();
+    }
+}
+
