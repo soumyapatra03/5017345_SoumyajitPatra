9922667bd665b5849878e72763b893f4e4d3a278
Soumyajit Patra <soumyajitpatra72@gmail.com>
2024-08-14 04:08:46 +0530

    Delete week3 directory

--- week3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.EmployeeManagementSystem;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    Department findByName(String name);
-}
